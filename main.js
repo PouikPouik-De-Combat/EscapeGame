1ec3a76c6ed55d3d840070a9292628c5de731076
Jonathan DUMONT <jonathan.dumont@etu.univ-smb.fr>
2022-12-01 19:45:07 +0100

    structure de base

---
+++ main.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+const app = express();
+const pathHTML = "./www/";
+const PORT = 8081;
+
+
+app.use(express.static(pathHTML));
+
+app.get("./", (req,res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+	res.sendFile(__dirname + pathHTML +'/index.html');
+});
+
+
+
+app.listen(PORT, () => {
+	console.log("Serveur listen on : "+PORT)
+});

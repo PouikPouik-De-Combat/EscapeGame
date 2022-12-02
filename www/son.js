f4e89c3e3fb7258e40b401de7d9aa2770d7f5558
Akane277353 <39272953+Akane277353@users.noreply.github.com>
2022-12-02 03:04:08 +0100

    le son

---
+++ www/son.js
@@ -0,0 +1,33 @@
+
+var proute = new Audio("proute.mp3");
+proute.oncanplaythrough = function ( ) { }
+proute.onended = function ( ) { }
+
+var adroite = new Audio("adroite.mp3");
+adroite.oncanplaythrough = function ( ) { }
+adroite.onended = function ( ) { }
+
+var againtoujourpasla = new Audio("againtoujourpasla.mp3");
+auagaintoujourpasladio.oncanplaythrough = function ( ) { }
+againtoujourpasla.onended = function ( ) { }
+
+var agauche = new Audio("agauche.mp3");
+agauche.oncanplaythrough = function ( ) { }
+agauche.onended = function ( ) { }
+
+var plusvite = new Audio("plusvite.mp3");
+plusvite.oncanplaythrough = function ( ) { }
+plusvite.onended = function ( ) { }
+
+var suuu = new Audio("suuu.mp3");
+suuu.oncanplaythrough = function ( ) { }
+suuu.onended = function ( ) { }
+
+var toujourpasla = new Audio("toujourpasla.mp3");
+toujourpasla.oncanplaythrough = function ( ) { }
+toujourpasla.onended = function ( ) { }
+
+var agauche2 = new Audio("agauche2.mp3");
+agauche2.oncanplaythrough = function ( ) { }
+agauche2.onended = function ( ) { }
+
